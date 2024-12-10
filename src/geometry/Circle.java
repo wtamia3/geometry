5419e3a6a8095b536a486de82809e8380012be80
wtamia3 <157857380+wtamia3@users.noreply.github.com>
2024-12-10 12:03:29 -0800

    Create Circle.java

---
+++ src/geometry/Circle.java
@@ -0,0 +1,24 @@
+package geometry;
+
+public class Circle extends Geometry2D {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle [radius=" + radius + "]";
+    }
+}
