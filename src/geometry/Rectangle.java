74d7e1e2471fc0fd0cb339b7322dbc29376f2f5e
wtamia3 <157857380+wtamia3@users.noreply.github.com>
2024-12-10 12:09:11 -0800

    Create Rectangle.java

---
+++ src/geometry/Rectangle.java
@@ -0,0 +1,26 @@
+package geometry;
+
+public class Rectangle extends Geometry2D {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double area() {
+        return width * height;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * (width + height);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle [width=" + width + ", height=" + height + "]";
+    }
+}
